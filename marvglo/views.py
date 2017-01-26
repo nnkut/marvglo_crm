5f995ca097f6a131324f11d68adcf169488385f5
Vitalii Protsenko <vp412@ic.ac.uk>
2017-01-26 15:43:41 +0000

    Implement transaction deletion, amend and view

--- marvglo/views.py
+++ marvglo/views.py
@@ -1,15 +1,14 @@
 import registration
 from django.dispatch import receiver
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.decorators.http import require_GET, require_POST
 import Queue
 
-from marvglo.models import Employee, SaleItem
+from marvglo.models import Employee, SaleItem, Transaction
 
 
 @require_GET
 def index(request):
-
     # Collect transactions for lower levels
     employee = request.user.employee
     transactions = []
@@ -17,11 +16,11 @@ def index(request):
     subEmployeeQueue = Queue.PriorityQueue()
     for sub_emp in list(employee.employee_set.all()):
         subEmployeeQueue.put(sub_emp)
-    #subEmployeeQueue.put(list(employee.employee_set.all()))
+    # subEmployeeQueue.put(list(employee.employee_set.all()))
     while not subEmployeeQueue.empty():
         sub_employee = subEmployeeQueue.get()
         transactions.extend(list(sub_employee.transaction_set.all()))
-        #subEmployeeQueue.put(list(sub_employee.employee_set.all()))
+        # subEmployeeQueue.put(list(sub_employee.employee_set.all()))
         for sub_emp in list(sub_employee.employee_set.all()):
             subEmployeeQueue.put(sub_emp)
 
@@ -42,19 +41,55 @@ def register_new_player(sender, **kwargs):
 
 @require_GET
 def remove_transaction(request, transaction_id):
-    pass
+    try:
+        t = Transaction.objects.get(id=transaction_id)
+        # check transaction ownership
+        if t.owner == request.user.employee:
+            t.delete()
+    except Transaction.DoesNotExist:
+        # did not even exist
+        pass
+    return redirect(index)
 
 
 @require_GET
 def view_transaction(request, transaction_id):
-    pass
+    ctx = {
+        'isAuthenticated': request.user.is_authenticated,
+        'adminApproved': request.user.employee.admin_approved,
+        'items': list(SaleItem.objects.all())
+    }
+    try:
+        t = Transaction.objects.get(id=transaction_id)
+        # check transaction ownership
+        if t.owner != request.user.employee:
+            return redirect(index)
+        ctx['transaction'] = t
+    except Transaction.DoesNotExist:
+        # did not even exist
+        return redirect(index)
+    return render(request, 'marvglo/transaction.html', ctx)
 
 
 @require_POST
 def submit_transaction(request):
-    pass
+    t = Transaction(item=SaleItem.objects.get(name=request.POST['itemName']),
+                    quantity=request.POST['quantity'],
+                    owner=request.user.employee)
+    t.save()
+    return redirect(index)
 
 
 @require_POST
 def amend_transaction(request, transaction_id):
-    pass
+    try:
+        t = Transaction.objects.get(id=transaction_id)
+        # check transaction ownership
+        if t.owner == request.user.employee:
+            t.item = SaleItem.objects.get(name=request.POST['itemName'])
+            t.quantity = request.POST['quantity']
+            t.save()
+    except Transaction.DoesNotExist:
+        # did not even exist
+        pass
+    return redirect(index)
