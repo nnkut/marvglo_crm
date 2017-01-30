b8e8b98230342dad52d0edcdb08b2ce9759f8b11
Vitalii Protsenko <vp412@ic.ac.uk>
2017-01-30 13:01:04 +0000

    Implement total sales, num sales and commission calculations and display on page load

--- marvglo/static/js/main.js
+++ marvglo/static/js/main.js
@@ -1,3 +1,38 @@
 $(document).ready(function () {
     $('#transactionStats').tablesorter();
+
+    updateTotalsFromTable();
 });
+
+
+function updateTotalsFromTable() {
+    var num_sales = [];
+    var sale_prices = [];
+    var commissions = [];
+    var totalNumSales = 0;
+    var totalSales = 0;
+    var totalCommission = 0;
+
+    $('#transactions-form-data tr').each(function (idx, row) {
+        if ($(row).is(':visible')) {
+            var cells = $(row).children();
+            num_sales.push($(cells[3]).html());
+            sale_prices.push($(cells[2]).html());
+            commissions.push($(cells[5]).html());
+        }
+    });
+
+    for (var i = 0; i < num_sales.length; ++i) {
+        var sale = parseFloat(sale_prices[i]);
+        var num_sale = parseInt(num_sales[i], 10);
+        totalCommission += parseFloat(commissions[i]);
+        totalSales += sale * num_sale;
+        totalNumSales += num_sale;
+    }
+
+    // shove results onto the page
+    $('#total-num-sales').html(totalNumSales);
+    $('#total-commission').html(totalCommission);
+    $('#total-sales').html(totalSales);
+
+}
